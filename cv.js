11574cf6f732cf937c9c1355643a5b6081b6aff1
jbcazaux <jbcazaux@gmail.com>
2013-12-20 00:32:06 +0100

    add more description

--- cv.js
+++ cv.js
@@ -2,8 +2,7 @@
 
     function addClass(classname, element ) {
         var cn = element.className;
-        var rxp = new RegExp( "\\s?\\b"+classname+"\\b", "g" );
-
+        var rxp = new RegExp( "\\s+\\b"+classname+"\\b", "g" );
         if( !cn.match(rxp) ) {
             classname = ' ' + classname;
             element.className = cn + classname;
@@ -11,9 +10,18 @@
     }
 
     function removeClass( classname, element ) {
+        var rxp = new RegExp( "\\s+\\b"+classname+"\\b", "g" );
         var cn = element.className;
-        var rxp = new RegExp( "\\s?\\b"+classname+"\\b", "g" );
         element.className = cn.replace( rxp, '' );
+    }
+
+    function toggleClass(classname, element){
+        var rxp = new RegExp( "\\s+\\b"+classname+"\\b", "g" );
+        if (element.className.match(rxp)){
+            element.className = element.className.replace( rxp, '' );
+        } else {
+            element.className = element.className + ' ' + classname;
+        }
     }
 
     function matchFilter(tags, filters){
@@ -55,28 +63,28 @@
 
     function registerEvents(){
         var displayOld = document.getElementById('displayOld');
-        var hideOld = document.getElementById('hideOld');
         var oldies = document.getElementsByClassName('old mission');
 
         displayOld.onclick = function(){
             for (var i = 0 ; i < oldies.length; i++){
-                removeClass("hiddenOld", oldies[i]);
+                toggleClass("hiddenOld", oldies[i]);
             }
-            displayOld.className = "displayOld switch hidden"
-            hideOld.className = "hideOld switch";
-        }
-        hideOld.onclick = function(){
-            for (var i = 0 ; i < oldies.length; i++){
-                addClass("hiddenOld", oldies[i]);
-            }
-            displayOld.className = "displayOld switch"
-            hideOld.className = "hideOld switch hidden";
+            displayOld.innerHTML = displayOld.innerHTML.indexOf("Afficher") > -1 ?
+                   "Masquer les missions plus anciennes" : "Afficher les missions plus anciennes";
         }
 
         var checkboxes = document.querySelectorAll('input[type=checkbox]');
         for (var i = 0 ; i < checkboxes.length; i++){
             checkboxes[i].onclick = onCbClick;
         }
+
+        var descriptionPlus = document.getElementById('plus');
+        var descriptionMore = document.getElementById('more');
+        plus.onclick = function(){
+            plus.innerHTML = plus.innerHTML.indexOf("Plus") > -1 ? "Réduire" : "Plus...";
+            toggleClass("hidden", descriptionMore);
+        }
+
 
 
     };
