c214e9bfb17ceb406825079d5ce98d736b90e72b
jb cazaux <jbcazaux@gmail.com>
2019-01-18 01:25:23 +0100

    remove manifest

--- cv.js
+++ cv.js
@@ -150,23 +150,6 @@
         };
     }
 
-    window.addEventListener('load', function(e) {
-        if (window.applicationCache) {
-            window.applicationCache.addEventListener('updateready', function(e) {
-                if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
-                    // Browser downloaded a new app cache.
-                    // Swap it in and reload the page to get the new hotness.
-                    window.applicationCache.swapCache();
-                    if (confirm('A new version of this site is available. Load it?')) {
-                        window.location.reload();
-                    }
-                } else {
-                    // Manifest didn't changed. Nothing new to server.
-                }
-            }, false);
-        }
-    }, false);
-
     window.onload = registerEvents;
 })();
 
